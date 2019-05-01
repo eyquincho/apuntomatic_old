c5957cb01630f7d9339f71f5907dd93211397366
Quincho <joaquin@muruais.com>
2019-05-01 22:56:39 +0200

    pequeños cambios

--- new-ap/js/tablas-apuntomatic.js
+++ new-ap/js/tablas-apuntomatic.js
@@ -2,11 +2,28 @@
 $(document).ready(function() {
   $('#dataTable').DataTable( {
 	"language": {
-            "lengthMenu": "Mostrar _MENU_ resultados por página",
-            "zeroRecords": "No se han encontrado resultados",
-            "info": "Página _PAGE_ de _PAGES_",
-            "infoEmpty": "No se han encontrado resultados",
-            "infoFiltered": "(fltrado de _MAX_ resultados totales)"
-        }
+    "sProcessing":     "Procesando...",
+    "sLengthMenu":     "Mostrar _MENU_ registros",
+    "sZeroRecords":    "No se encontraron resultados",
+    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+    "sInfoPostFix":    "",
+    "sSearch":         "Buscar:",
+    "sUrl":            "",
+    "sInfoThousands":  ",",
+    "sLoadingRecords": "Cargando...",
+    "oPaginate": {
+        "sFirst":    "Primero",
+        "sLast":     "Último",
+        "sNext":     "Siguiente",
+        "sPrevious": "Anterior"
+    },
+    "oAria": {
+        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+    }
+}
   });
 });
